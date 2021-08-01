bffba466481969e0c2bbb7b293a705bebe3e9647
Mehrab Bozorgi <mehrab.bozorgi@gmail.com>
2021-08-01 11:04:43 +0430

    flutter_cookie_store_ui

---
+++ android/app/src/main/java/com/example/flutter_cookie_store_ui/MainActivity.java
@@ -0,0 +1,6 @@
+package com.example.flutter_cookie_store_ui;
+
+import io.flutter.embedding.android.FlutterActivity;
+
+public class MainActivity extends FlutterActivity {
+}
---
+++ lib/model/product.dart
@@ -0,0 +1,15 @@
+class Product {
+  final int id;
+  final String name;
+  final String image;
+  final String description;
+  final double price;
+
+  Product({
+    required this.id,
+    required this.name,
+    required this.image,
+    required this.description,
+    required this.price,
+  });
+}
